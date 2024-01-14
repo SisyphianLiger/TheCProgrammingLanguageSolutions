5cd76c8b47424dc66a7570ce704bd24f7c187c05
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-01-14 11:20:52 +0100

    Completed 2.1 (Without calculations)

--- 2_KnR/2_1/2_1.c
+++ 2_KnR/2_1/2_1.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <limits.h>
+
+/* char, short, int, long, signed, unsigned */
 
 int main () {
+    // Chars
+    printf("CHAR_MAX is: %i\n", CHAR_MAX);
+    printf("CHAR_MIN is: %i\n", CHAR_MIN);
+
+    // Short
+    printf("SHORT_MAX is: %d\n", SHRT_MAX);
+    printf("SHORT_MIN is: %d\n", SHRT_MIN);
     
-    printf("Hello Chapter 2");
+    // Int - Signed
+    printf("INT_MAX is: %i\n", INT_MAX);
+    printf("INT_MIN s: %i\n", INT_MIN);
+
+    // Long
+    printf("LONG_MAX s: %ld\n", LONG_MAX);
+    printf("LONG_MIN is: %ld\n", LONG_MIN);
+
+    // Unsigned
+    printf("USIGNED MAX is: %u\n", UINT_MAX);
+    printf("USIGNED MIN is: %u\n", 0);
+
     return 0;
 
 }
