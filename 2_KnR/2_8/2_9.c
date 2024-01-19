62f9de40e09cf4e8e8c56d10a2bf40e522e7d4b8
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-01-19 21:50:00 +0100

    Completed 2_8

---
+++ 2_KnR/2_8/2_9.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <stdio.h>
+
+
+int rightrot(int x, int n);
+
+int main () {
+    printf("Rotate 32 to 8 == Answer: %i\n", rightrot(32, 2));
+    printf("Rotate 32 to 0 == Answer: %i\n", rightrot(32, 6));
+    printf("Rotate 256 to 32 == Answer: %i\n", rightrot(256, 3));
+
+    return 0;
+}
+
+int rightrot(int x, int n) {
+    return x >> n;
+
+}
