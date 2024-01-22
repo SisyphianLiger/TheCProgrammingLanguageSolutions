f6c0a055bc0d0a808db97274e69d6624815576dd
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-01-22 22:36:29 +0100

    assignment 3_2 completed

--- 3_KnR/3_2/3_2.c
+++ 3_KnR/3_2/3_2.c
@@ -1,13 +1,54 @@
 #include <stdio.h>
-
 #define BUFFER 1000
 
 
+void escape(char s[], char t[]);
 
 int main() {
 
     char s[BUFFER];
+    char t[BUFFER];
 
+    escape(s, t);
+
+    int i = 0;
+    printf("\n");
+    while (t[i] != '\0') {
+        printf("%c", t[i]);
+        i++;
+    }
 
     return 0;
 }
+
+
+void escape(char s[], char t[]){
+
+    int c, i, j;
+    i = 0;
+    while ((c = getchar()) != EOF) {
+        s[i] = c;
+        i++;
+    }
+    s[++i] = '\0';
+
+    i = 0, j = 0;
+
+    while(s[i] != '\0'){
+        switch (s[i]) {
+            case '\t':
+                t[j++] = '\\';
+                t[j++] = 't';
+                break;
+            case '\n':
+                t[j++] = '\\';
+                t[j++] = 'n';
+                break;
+            default:
+                t[j++] = s[i];
+                break;
+        }
+        i++;
+    }
+    t[j++] = '\0';
+} 
